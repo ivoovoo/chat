c417463994bad3e5847e63ef83913e9c65e5386b
Gfhjkmfl@1@1 <kostechkina@gmail.com>
2025-03-31 11:39:37 +0400

    add

--- src/widget/Header/ui/NavItem.jsx
+++ src/widget/Header/ui/NavItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Sprite from "../../../shared/ui/Sprite/Sprite";
-import { classNames } from "../../../shared/lib/classNames/classNames";
+import {classNames} from "../../../shared/lib/classNames/classNames";
 
-const NavItem = ({ children, icon, className, ...other }) => {
-  return (
-    <Link className={classNames("header__link", [className])} {...other}>
-      {children}
-      <Sprite icon={icon} width={24} height={24} />
-    </Link>
-  );
+const NavItem = ({ icon, valueFirst, valueSecond, active, ...other}) => {
+    return (
+        <Link className={classNames("header__link", [], {active})}  {...other}>
+            {active ? valueSecond : valueFirst}
+            <Sprite icon={icon} width={24} height={24}/>
+        </Link>
+    );
 };
 
 export default NavItem;
--- src/widget/Header/ui/Navigation.jsx
+++ src/widget/Header/ui/Navigation.jsx
@@ -2,19 +2,31 @@ import React from "react";
 import NavItem from "./NavItem";
 
 const Navigation = () => {
-  return (
-    <nav className="header__nav">
-      <NavItem className={"first"} to="/" icon={"lightning"}>
-        Upgrade Plan
-      </NavItem>
-      <NavItem to="/" icon={"help-circle"}>
-        Help
-      </NavItem>
-      <NavItem to="/" icon={"link-angled"}>
-        API
-      </NavItem>
-    </nav>
-  );
+    const [links, setLinks] = React.useState([
+        {icon: 'lightning', valueFirst: 'Upgrade Plan',valueSecond:'Max Plan', to: '/', active: true},
+        {icon: 'help-circle', valueFirst: 'Help', valueSecond:'ainowgo@gmail.com', to: '/'},
+        {icon: 'link-angled', valueFirst: 'API', valueSecond:'ainowgo@gmail.com', to: '/'}
+    ]);
+
+    const handleClick = (i) => {
+        const newLinks =  links.map((link, index) => {
+            if(i === index) link.active = true;
+            else link.active = false
+            return link;
+        })
+
+        setLinks(newLinks);
+    }
+    return (
+        <nav className="header__nav">
+
+            {links.map((link, index) => (<NavItem to={link.to} icon={link.icon} valueSecond={link.valueSecond}
+                                         valueFirst={link.valueFirst} active={link.active}
+                                         onClick={() => handleClick(index)}
+            />))}
+
+        </nav>
+    );
 };
 
 export default Navigation;
